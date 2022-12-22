fee3b372c3635e1275d56f4f1c0641cbebb312e8
CarlosArthurDev <103152440+CarlosArthurDev@users.noreply.github.com>
2022-12-22 14:26:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+function setup(){
+   canvas=createCanvas(450,450)
+   canvas.position(750,200)
+   video= createCapture(VIDEO)
+   video.size(400,400)
+   video.position(70,200)
+}
+
+function modelLoaded(){
+   console.log("seu modelo foi carregado")
+}
+
+function draw(){
+
+}
