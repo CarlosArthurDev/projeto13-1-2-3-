d8e9143331ec4a0708c905925823eb3c8a7cade4
CarlosArthurDev <103152440+CarlosArthurDev@users.noreply.github.com>
2022-12-22 23:49:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,39 @@
-
-
-
+var modelStatus=""
+function preload(){
+    img=loadImage("dog_cat.jpg")
+}
 function setup(){
-   canvas=createCanvas(450,450)
-   canvas.position(750,200)
-   video= createCapture(VIDEO)
-   video.size(400,400)
-   video.position(70,200)
+    canvas=createCanvas(640,420)
+    canvas.center()
+
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded)
+    document.getElementById("status").innerHTML="status detectando objetos."
+}
+function draw(){
+    image(img,0,0,640,420)
+    fill("#FF0000")
+    text("dog",45,75)
+    stroke("#FF0000")
+    noFill()
+    rect(30,60,450,350)
+
+    fill("#FF0000")
+    text("cat",320,120)
+    stroke("#FF0000")
+    noFill()
+    rect(300,70,270,450)
 }
 
 function modelLoaded(){
-   console.log("seu modelo foi carregado")
-}
+    console.log("modelo carregado")
+    modelStatus=true
+    objectDetector.detect(img,gotResult)
 
-function draw(){
-
 }
+function gotResult(error,results){
+    if(error){  
+        console.log(error)
+    }
+    console.log(results)
+    
+} 
